663ce4ec5412d45f0375fe06823a7eeae71efbde
Andrew Brady <andrew.brady@graze.com>
2020-07-10 16:34:47 +0100

    change API key

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ weather.temperature = {
 
 const KELVIN = 273
 
-const key = "82005d27a116c2880c8f0fcb866998a0"
+const key = "18198015a94673f46a4c9e60190d8315"
 
 //CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
