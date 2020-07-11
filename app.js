eb3dd4887e8833f8f9c2121db590efe0a668ab0a
Andrew Brady <andrew.brady@graze.com>
2020-07-11 18:19:05 +0100

    implemented a typeahead dropdown which allows the user to enter the city by clicking on the the suggested city.

--- app.js
+++ app.js
@@ -138,3 +138,52 @@ document.getElementById("form").onkeypress = function(e) {
     e.preventDefault();
   }
 }
+
+// Code for the typeahead feature
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    // here we need to figure out if the city or state matches what was searched
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+  });
+}
+
+
+
+function displayMatches() {
+
+  suggestions.style.display = "block";
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+  listItems = document.getElementsByTagName('li')
+    
+   for(var i = 0; i < listItems.length; i++) {
+       listItems[i].addEventListener("click", function(){
+           
+        searchInput.value =this.innerText;
+
+ });
+}
+}
+const searchInput = document.getElementById('input');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
